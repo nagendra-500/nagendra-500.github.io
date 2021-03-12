e977181823e8341a98df3f7a04a5c6578c8f1f09
nagendra-500 <satya.nagendra@500apps.com>
2021-03-12 10:34:26 +0530

    Test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,3 +1,6 @@
 // if ("undefined" === typeof window) {
-  importScripts("https://pushly.s3.ap-south-1.amazonaws.com/lib/firebase-listener_new.js");
+  // importScripts("https://pushly.s3.ap-south-1.amazonaws.com/lib/firebase-listener_new.js");
 // }
+
+importScripts("https://cdn-push.500apps.com/lib/firebase-listener.js");
+
