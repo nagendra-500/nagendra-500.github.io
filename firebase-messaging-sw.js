5e415e3e6686ede4dccc2226adc701ccef6fe936
nagendra-500 <satya.nagendra@500apps.com>
2021-03-12 08:04:53 +0530

    test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,3 +1,3 @@
 if ("undefined" === typeof window) {
-  importScripts("https://pushly.s3.ap-south-1.amazonaws.com/lib/firebase-listener1.js");
+  importScripts("https://pushly.s3.ap-south-1.amazonaws.com/lib/firebase-listener_new.js");
 }
