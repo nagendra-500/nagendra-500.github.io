dd4b8ab6465edb6dbebe9f4bcdaf59c9a3cdc503
nagendra-500 <satya.nagendra@500apps.com>
2021-03-14 13:04:47 +0530

    ds

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -4,55 +4,51 @@
 // importScripts("https://cdn-push.500apps.com/lib/firebase-listener.js");
 
 
-importScripts("https://www.gstatic.com/firebasejs/6.4.2/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/6.4.2/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/6.6.2/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/6.6.2/firebase-messaging.js");
 
 /**
  * @class PushlyFirebaseListener
  */
-class PushlyFirebaseListener {
-
+ class PushlyFirebaseListener {
   /**
    * @constructor
    */
   constructor() {
+    // _500Client.rest.setServerURL(`${window._pushGlobal.serverUrl}`);
+
     // Store messageApi of current executed message when user clicks on the message
-    this.exeMessageApi = '';
-    
-    // Store jwt token which has domainId, flowId, messageId 
-    this.messageApi = '';
-    
+    this.exeMessageApi = "";
+
+    // Store jwt token which has domainId, flowId, messageId
+    this.messageApi = "";
+
     // Fcm subscription object
     this.subscriptionObject = {};
-    
-    // Store event action url 
-    this.url = '';
-    
-    // Store current domain url 
-    this.launchUrl = '';
+
+    // Store event action url
+    this.url = "";
+
+    // Store current domain url
+    this.launchUrl = "";
   }
 
   /**
    * Initialization method
    */
   init() {
-
     // To listen the messages pushed from service worker
-    self.addEventListener('push', function (event) {
+    self.addEventListener("push", function (event) {
+      console.log("event", event);
       var message = event.data.json();
       this.messageApi = message.data.message_api;
       this.subscriptionObject = message.data.subscription_object;
       this.launchUrl = message.data.launch_url;
-      var region = message.data.region;
       var obj = JSON.parse(message.data.notification);
       const title = obj.title;
       const options = {
         body: obj.body,
         icon: obj.icon,
-        data: {
-          message_api: message.data.message_api,
-          region:region
-        },
       };
       if (message.data.action_button) {
         options["actions"] = JSON.parse(message.data.action_button);
@@ -62,59 +58,58 @@ class PushlyFirebaseListener {
     });
 
     // To listen when user clicks on notification
-    self.addEventListener('notificationclose', (event) => {
+    self.addEventListener("notificationclose", (event) => {
+      console.log("notificationclose", event);
       const clickedNotification = event.notification;
-      console.log('event.action',event.action);
-      if (clickedNotification.data.message_api != this.exeMessageApi) {
-        //saveUserAction("close");
-        PushlyFirebaseListener.saveUserAction("close",clickedNotification.data.message_api,clickedNotification.data.region);
-
+      if (this.messageApi != this.exeMessageApi) {
+        saveUserAction("close");
       }
     });
 
     // To listen when user closes notification
-    self.addEventListener('notificationclick', function (event) {
+    self.addEventListener("notificationclick", function (event) {
+      console.log("notificationclick", event);
+
       if (event.action) {
         PushlyFirebaseListener.url = event.action;
-        clients.openWindow(event.action)
+        clients.openWindow(event.action);
       } else {
         PushlyFirebaseListener.url = PushlyFirebaseListener.launchUrl;
-        clients.openWindow(launch_url)
+        clients.openWindow(launch_url);
       }
 
       this.exeMessageApi = this.messageApi;
       const clickedNotification = event.notification;
-      PushlyFirebaseListener.saveUserAction(((event.action) ? event.action : "executed"),clickedNotification.data.message_api,clickedNotification.data.region);
-      
-      // Reset variable
-      this.exeMessageApi = '';
+      console.log("PushlyFirebaseListener", PushlyFirebaseListener);
+      PushlyFirebaseListener.saveUserAction(event.action ? event.action : "executed");
 
+      // Reset variable
+      this.exeMessageApi = "";
     });
   }
 
   /**
    * To make a network call and store messages in database
    */
-  static saveUserAction = function (actionText,message_api,region) {
+  static saveUserAction(actionText, message_api, region) {
+    console.log("saveUserAction", saveUserAction);
     const messagelog = {
       url: this.url,
       executionText: actionText,
-      message_api: message_api,
-      subscriptionObject: this.subscriptionObject
-    }
+      message_api: this.messageApi,
+      subscriptionObject: this.subscriptionObject,
+    };
 
-    var messageLogUrl = 'https://custom.'+region+'.500apps.io/c/pushly/messagelog';
-
-    fetch(messageLogUrl, {
+    fetch("https://custom.dev.500apps.io/c/pushly/messagelog", {
       method: "post",
       headers: {
         Accept: "application/json",
       },
       body: JSON.stringify(messagelog),
-    })
+    });
   }
 }
 (() => {
-  var _pushlyFirebaseListener = new PushlyFirebaseListener()
+  var _pushlyFirebaseListener = new PushlyFirebaseListener();
   _pushlyFirebaseListener.init();
 })();
