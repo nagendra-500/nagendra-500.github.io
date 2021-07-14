7bea048a5ab2fb5ddc54e679d2caecdbd2b9f95f
nagendra-500 <satya.nagendra@500apps.com>
2021-07-14 16:36:27 +0530

    Updated Script

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,2 +1,6 @@
-importScripts("https://infinity-public-js.500apps.com/push/listener.min.js");
+// importScripts("https://infinity-public-js.500apps.com/push/listener.min.js");
+
+
+importScripts("https://pushly.s3.ap-south-1.amazonaws.com/lib/listener.min.js");
+
   
